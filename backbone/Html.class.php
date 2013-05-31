ebc8a243699e130672496d8493f6f338731a8073
James Tracy <james.a.tracy@gmail.com>
2013-05-31 17:14:42 -0600

    Update Html.class.php
    phpdoc comment syntax

--- backbone/Html.class.php
+++ backbone/Html.class.php
@@ -14,136 +14,146 @@
 */
 class Html
 {
-	/*
-	Constructs an html link, or anchor tag
-	
-	@param [string] $url The url. If it begins with a '/' then it is considered relative to the root, else an absolute path.
-	@param [string] $text The inner text of the link
-	@param [array] $attributes A list of key => value pairs
-	@return [string] The html
-	*/
+	/**
+	 * Constructs an html link, or anchor tag
+	 * 
+	 * @since 0.1.0
+	 * @param string $url The url. If it begins with a '/' then it is considered relative to the root, else an absolute path.
+	 * @param string $text The inner text of the link
+	 * @param array $attributes A list of key => value pairs
+	 * @return string The html
+	 */
 	public function link($url, $text, $attributes = array())
 	{
 		$request = Backbone::$request;
 
-		if(substr($url, 0, 1) == "/")
+		if(substr($url, 0, 1) == "/") {
 			// relative path
 			$url = $request->link($url);
+		}
 		
 		$attributes["href"] = $url;
 		return $this->tag("a", $text, $attributes);
 	}
 	
-	/*
-	Constructs an image tag
-	
-	@param [string] $url The source url. If it begins with a '/' then it is considered relative to the root, else an absolute path.
-	@param [array] $attributes A list of key => value pairs
-	@return [string] The html
-	*/
+	/**
+	 * Constructs an image tag
+	 * 
+	 * @since 0.1.0
+	 * @param string $url The source url. If it begins with a '/' then it is considered relative to the root, else an absolute path.
+	 * @param array $attributes A list of key => value pairs
+	 * @return string The html
+	 */
 	public function image($url, $attributes = array())
 	{
 		$request = Backbone::$request;
 
-		if(substr($url, 0, 1) == "/")
+		if(substr($url, 0, 1) == "/") {
 			// relative path
 			$url = $request->link($url);
+		}
 		
 		$attributes["src"] = $url;
 		return $this->tag("img", null, $attributes);
 	}
 	
-	/*
-	Constructs a script tag
-	
-	@param [string] $url The external source of the script. 
-		If it begins with a '/' then it is considered relative to the root, else an absolute path.
-	@param [array] $attributes A list of key => value pairs
-	@param [boolean] $inline If true, the script is inserted inline. $url must be relative to the root.
-	@return [string] The html
-	*/
+	/**
+	 * Constructs a script tag
+	 * 
+	 * @since 0.1.0
+	 * @param string $url The external source of the script. 
+	 * 	If it begins with a '/' then it is considered relative to the root, else an absolute path.
+	 * @param array $attributes A list of key => value pairs
+	 * @param bool $inline If true, the script is inserted inline. $url must be relative to the root.
+	 * @return string The html
+	 */
 	public function script($url, $attributes = array(), $inline = false)
 	{
 		$request = Backbone::$request;
 		
 		$attributes["type"] = "text/javascript";
 		
-		if($inline)
-		{
-			if(substr($url, 0, 1) == "/")
-			{
+		if($inline) {
+			if(substr($url, 0, 1) == "/") {
 				$url = substr($url, 1);
-				if(file_exists(ABSPATH.$url))
-				{
+				if(file_exists(ABSPATH.$url)) {
 					return $this->tag("script", "\n".file_get_contents(ABSPATH.$url)."\n", $attributes)."\n";
 				}
 			}
 			return "";
-		}
-		else
-		{
-			if(substr($url, 0, 1) == "/")
+		} else {
+			if(substr($url, 0, 1) == "/") {
 				// relative path
 				$url = $request->link($url);
+			}
 			
 			$attributes["src"] = $url;
 			return $this->tag("script", "", $attributes)."\n";
 		}
 	}
 	
-	/*
-	Constructs a link tag to a stylesheet
-	
-	@param [string] $url The external source of the stylesheet. 
-		If it begins with a '/' then it is considered relative to the root, else an absolute path.
-	@param [array] $attributes A list of key => value pairs
-	@return [string] The html
-	*/
+	/**
+	 * Constructs a link tag to a stylesheet
+	 * 
+	 * @since 0.1.0
+	 * @param string $url The external source of the stylesheet. 
+	 * 	If it begins with a '/' then it is considered relative to the root, else an absolute path.
+	 * @param array $attributes A list of key => value pairs
+	 * @return string The html
+	 */
 	public function stylesheet($url, $attributes = array())
 	{
 		$request = Backbone::$request;
 
-		if(substr($url, 0, 1) == "/")
+		if(substr($url, 0, 1) == "/") {
 			// relative path
 			$url = $request->link($url);
+		}
 		
 		$attributes["href"] = $url;
 		$attributes["type"] = "text/css";
 		$attributes["rel"] = "stylesheet";
 		return $this->tag("link", null, $attributes)."\n";
 	}
-	/*
-	Generic function used for constructing any tag open/close combination
 	
-	@param [string] $name The name of the tag (ex: "img")
-	@param [string] $inner Optional. The inner contents of the tag. If this is null, then the tag self closes (ex: <br/>)
-	@param [array] $attributes Optional. A list of attributes defined as key => value pairs.
-	@return [string] The tag's html
-	*/
+	/**
+	 * Generic function used for constructing any tag open/close combination
+	 * 
+	 * @since 0.1.0
+	 * @param string $name The name of the tag (ex: "img")
+	 * @param string $inner Optional. 
+	 * 	The inner contents of the tag. If this is null, then the tag self closes (ex: <br/>)
+	 * @param array $attributes Optional. A list of attributes defined as key => value pairs.
+	 * @return string The tag's html
+	 */
 	public function tag($name, $inner = null, $attributes = array())
 	{
 		$ob = '<'.$name.$this->_buildAttributes($attributes);
-		if($inner === null)
+		if($inner === null) {
 			$ob .= ' />';
-		else
+		} else {
 			$ob .= '>'.$inner.'</'.$name.'>';
+		}
 		return $ob;
 	}
 	
-	/*
-	Internal functinon for building a string of attributes from an associated array
-	
-	@param [array] $attributes Associated array of key => value pairs
-	@return [string] The string of attributes
-	*/
+	/**
+	 * Internal functinon for building a string of attributes from an associated array
+	 *
+	 * @since 0.1.0
+	 * @param array $attributes Associated array of key => value pairs
+	 * @return string The string of attributes
+	 */
 	protected function _buildAttributes($attributes)
 	{
 		$ob = '';
 		$attrs = array();
-		foreach($attributes as $attr => $val)
+		foreach($attributes as $attr => $val) {
 			$attrs[] = $attr.'="'.$val.'"';
-		if(count($attrs) > 0)
+		}
+		if(count($attrs) > 0) {
 			$ob .= ' '.join($attrs, " ");
+		}
 		return $ob;
 	}
 };
